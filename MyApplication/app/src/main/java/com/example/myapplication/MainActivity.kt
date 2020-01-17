7a84896ada1fecfc5379862c02ea39145306b0ca
Imran Baig <imranbaig@hotmail.co.uk>
2020-01-17 13:58:36 +0100

    updated the layouts

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -11,6 +11,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         val view = findViewById<View>(R.id.textView1)
 
         view.setOnClickListener {
